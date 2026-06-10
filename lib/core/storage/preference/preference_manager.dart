a9a0fc3cda5c1f1a8be59ac1e0c987a8b06997d9
MAYUR CHAVDA <mayurchavda1210@gmail.com>
2026-06-10 19:10:15 +0530

    profile ane face register api calling  Api Calling

--- lib/core/storage/preference/preference_manager.dart
+++ lib/core/storage/preference/preference_manager.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferenceManager {
   static const String _tokenKey = "auth_token";
+  static const String _faceKey = "registered_face";
 
   static Future<bool> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
@@ -15,11 +16,27 @@ class PreferenceManager {
 
   static Future<bool> clearToken() async {
     final prefs = await SharedPreferences.getInstance();
+    await clearRegisteredFace();
     return prefs.remove(_tokenKey);
   }
 
   static Future<bool> hasToken() async {
     final token = await getToken();
     return token != null && token.isNotEmpty;
+  }
+
+  static Future<bool> saveRegisteredFace(String base64Image) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setString(_faceKey, base64Image);
+  }
+
+  static Future<String?> getRegisteredFace() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_faceKey);
+  }
+
+  static Future<bool> clearRegisteredFace() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.remove(_faceKey);
   }
 }
